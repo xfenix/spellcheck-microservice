1003bcee2309ad13d53cd3ee22b6f7d536390128
Denis Anikin <ad@xfenix.ru>
2022-06-08 05:23:22 +0300

    Toml parser

--- whole_app/misc_helpers.py
+++ whole_app/misc_helpers.py
@@ -1,6 +1,7 @@
 """Helper for parsing cache version."""
 import sys
 
+import toml
 from loguru import logger
 
 from .settings import SETTINGS
@@ -11,13 +12,9 @@ logger.add(sys.stdout, serialize=SETTINGS.structured_logging)
 
 def parse_version_from_local_file() -> str:
     """Parse version from version file."""
-    pyproject_lines: list[str] = SETTINGS.path_to_version_file.read_text().split("\n")
-    version_parts: list[str] = []
-    for one_line in pyproject_lines:
-        if "version" in one_line:
-            version_parts = one_line.split("=")
-            break
-    if len(version_parts) == 2:
-        return version_parts[1].strip().strip('"')
-    logger.warning("Can't parse version from pyproject.toml")
-    return ""
+    try:
+        pyproject_obj: dict = toml.loads(SETTINGS.path_to_version_file.read_text())
+        return pyproject_obj["tool"]["poetry"]["version"]
+    except toml.TomlDecodeError:
+        logger.warning("Cant parse version from pyproject")
+        return ""
