cca1c3c88a553cef8630a8774002f0b3b78b61df
Denis Anikin <ad@xfenix.ru>
2022-08-04 03:27:44 +0300

    Try to achieve additional performance through orjson

--- whole_app/views.py
+++ whole_app/views.py
@@ -3,6 +3,7 @@ import typing
 
 import fastapi
 from anyio import to_thread
+from fastapi.responses import ORJSONResponse
 
 from . import dictionaries, misc_helpers, models, spell
 from .auth import auth_via_api_key
@@ -41,7 +42,7 @@ def startup() -> None:
     misc_helpers.init_logger()
 
 
-@SPELL_APP.post(f"{SETTINGS.api_prefix}/check/", summary="Check spelling")
+@SPELL_APP.post(f"{SETTINGS.api_prefix}/check/", summary="Check spelling", response_class=ORJSONResponse)
 async def spell_check_main_endpoint(
     request_payload: models.SpellCheckRequest,
     spell_service: spell.SpellCheckService = fastapi.Depends(),
